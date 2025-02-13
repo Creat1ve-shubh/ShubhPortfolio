3d951f4bf468e092f9bece7b3ad97fdf6e016e9e
Creat1ve <shubhdayal.shrivastava@gmail.com>
2025-02-14 00:29:54 +0530

    changing file names

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,7 +5,7 @@ import banner from "../assets/banner.jpeg";
 const Hero = () => {
   return (
     <div className="ml-[10vh] my-[8vh] flex items-center space-x-8 rounded-lg">
-      <h1 className="text-7xl font-thin font-serif text-black">"lets get this rolling"</h1>
+      <h1 className="text-7xl font-thin font-serif text-black">lets get this rolling</h1>
       <div className="" >
         <Image src={banner} alt="banner" width={1500} height={1100} />
       </div>
--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
 import AboutMe from "./components/AboutMe";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="bg-white min-h-screen overflow-hidden">
       <Navbar />
-    <Hero />
-    <AboutMe />
-    <Projects />
+      <Hero />
+      <AboutMe />
+      <Projects />
 
-    <Footer />
+      <Footer />
     </div>
   );
 }
